bca36471e7eccd0248f092c2441f28bf4f9680b0
matteodinapoli <matteodinapoli@10.168.93.8>
2015-04-28 17:49:42 +0200

    trying to build the game map. To reset line and columns.

--- src/main/java/it/polimi/ingsw/DiNapoliDiNardo/Main.java
+++ src/main/java/it/polimi/ingsw/DiNapoliDiNardo/Main.java
@@ -1,5 +1,12 @@
 package it.polimi.ingsw.DiNapoliDiNardo;
+import it.polimi.ingsw.DiNapoliDiNardo.model.*;
 
 public class Main {
-
+		
+	public static void main( String[] args){
+		GalileiMap G = new GalileiMap();
+		G.buildmap();
+		System.out.println(G.toString());
+	}
+	
 }
