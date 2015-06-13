9c10bc76a8d82f3493446d3f03a3eed2738f39a8
matteodinapoli <matteodinapoli@10.168.93.8>
2015-06-14 01:53:15 +0200

    sector deck test

---
+++ src/test/java/it/polimi/ingsw/dinapolidinardo/model/deck/TestSectorDeck.java
@@ -0,0 +1,37 @@
+package it.polimi.ingsw.dinapolidinardo.model.deck;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import it.polimi.ingsw.dinapolidinardo.model.cards.Card;
+import it.polimi.ingsw.dinapolidinardo.model.cards.SectorCard;
+import it.polimi.ingsw.dinapolidinardo.model.decks.SectorDeck;
+
+
+public class TestSectorDeck {
+
+	private SectorDeck sectordeck;
+	
+	@Before
+	public void setUp(){
+		sectordeck = new SectorDeck();
+	}
+	
+	@Test
+	public void reShuffleTest(){
+		for (int i=0; i<25; i++)
+			sectordeck.drawCard();
+		sectordeck.drawCard();
+		assertEquals(sectordeck.getDeck().size(), 24);
+	}
+	
+	@Test
+	public void typeTest(){
+		Card card = sectordeck.drawCard();
+		assertTrue(card instanceof SectorCard);
+	}
+		
+	
+}
